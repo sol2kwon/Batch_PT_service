fa1263ec3f06700271c6379fdef7e8557cc4a861
sol2kwon <amorfatisole@gmail.com>
2023-04-10 22:01:34 +0900

    first commit

---
+++ src/main/java/batch_service/batch_service/BatchServiceApplication.java
@@ -0,0 +1,61 @@
+package batch_service.batch_service;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class BatchServiceApplication {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+
+	public BatchServiceApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+		this.jobBuilderFactory = jobBuilderFactory;
+		this.stepBuilderFactory = stepBuilderFactory;
+	}
+
+	//step 선언 , Tasklet 구현
+	@Bean
+	public Step passStep(){
+		return this.stepBuilderFactory.get("passStep") //step 이름 넣어줌
+				.tasklet(new Tasklet() {
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+						System.out.println("Execute PassStep");
+						return RepeatStatus.FINISHED; //종료 선언
+					}
+				}).build();
+	}
+
+	@Bean
+	public Job passJob(){
+		return this.jobBuilderFactory.get("passJob")
+				.start(passStep())
+				.build();
+
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/batch_service/batch_service/BatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package batch_service.batch_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
